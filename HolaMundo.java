affa9427f12aff057f1bc8e1177f71cb51d8bcf4
Pablo Parra <pxparra@espe.edu.ec>
2021-02-17 22:23:27 -0500

    Update HolaMundo.java
    Mary hice el cambio del ingreso aumnete un scanner

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,10 @@
 public class HolaMundo {
 public static void main(String[] args){
-System.out.println("Hola Mundo");
+Scanner ingreso=new Scanner(System.in);
+String mensaje="";
+mensaje=ingreso.next();
+System.out.println(mensaje);
 System.out.println("Mary");
 
 }
-
-
-
 }
